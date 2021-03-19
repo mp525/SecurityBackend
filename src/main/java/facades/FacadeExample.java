71bf050c769a59ee927396a01db6bd55219f0259
Mathias Parking <cph-mp525@cphbusiness.dk>
2021-03-19 10:49:36 +0100

    first commit

---
+++ src/main/java/facades/FacadeExample.java
@@ -0,0 +1,21 @@
+package facades;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class FacadeExample {
+
+    
+    //Private Constructor to ensure Singleton
+    private FacadeExample() {}
+    
+    
+    
+
+}
