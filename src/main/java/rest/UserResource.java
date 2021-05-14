ad4d00b1310e0a0bb1e35c03d3a21c1ff8b5250e
matti Hansen <MattiBSH>
2021-05-14 13:17:28 +0200

    post adjust backend

--- src/main/java/rest/UserResource.java
+++ src/main/java/rest/UserResource.java
@@ -190,15 +190,20 @@ public class UserResource {
     @Produces({MediaType.APPLICATION_JSON})
     @Consumes({MediaType.APPLICATION_JSON})
     @RolesAllowed("user")
-    public String addPost(String post) {
+    public String addPost(String post) throws IOException {
+        UserResource ur = new UserResource();
         PostDTO p = GSON.fromJson(post, PostDTO.class);
+        boolean human=ur.verify(p.getUser().getToken());
+        if(human){
         //ide indtil videre
-        String name =securityContext.getUserPrincipal().getName();
+        String name = securityContext.getUserPrincipal().getName();
         p.getUser().setUserName(name);
         //Virker det mon sikkerhedsmæssigt
         PostDTO result=postFacade.addPost(p);
         return GSON.toJson(result);
-        
+        }else{
+            throw new Error("Robo got hit by the Robocop");
+        }
     }
     
     
@@ -289,63 +294,37 @@ public class UserResource {
         return ""+human;
     }
     
-    public boolean CheckifHuman(String token) throws IOException{
-        String secret="6LcIFdAaAAAAAK_Iv5yhQXxqxxQhnESAHCS7UYr9";
-        String url = ("https://www.google.com/recaptcha/api/siteverify?secret"+secret+"&response="+token);
-        
-        System.out.println(url);
-        String response= HttpUtils.fetchData(url);
-        RecaptchaDTO data=GSON.fromJson(response, RecaptchaDTO.class);
-        System.out.println(response);
-        
-        return data.isSuccess();
-    }
-    public static final String url = "https://www.google.com/recaptcha/api/siteverify";
-	public static final String secret = "6LcIFdAaAAAAAK_Iv5yhQXxqxxQhnESAHCS7UYr9";
+    public final String url = "https://www.google.com/recaptcha/api/siteverify";
+	public final String SECRET = "6LcIFdAaAAAAAK_Iv5yhQXxqxxQhnESAHCS7UYr9";
 	private final static String USER_AGENT = "Mozilla/5.0";
 
-	public static boolean verify(String gRecaptchaResponse) throws IOException {
+	private boolean verify(String gRecaptchaResponse) throws IOException {
 		if (gRecaptchaResponse == null || "".equals(gRecaptchaResponse)) {
 			return false;
 		}
-		
 		try{
 		URL obj = new URL(url);
 		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-
 		// add reuqest header
 		con.setRequestMethod("POST");
 		con.setRequestProperty("User-Agent", USER_AGENT);
 		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-		String postParams = "secret=" + secret + "&response="
+		String postParams = "secret=" + SECRET + "&response="
 				+ gRecaptchaResponse;
-
 		// Send post request
 		con.setDoOutput(true);
 		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
 		wr.writeBytes(postParams);
 		wr.flush();
 		wr.close();
-
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + postParams);
-		System.out.println("Response Code : " + responseCode);
-
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 				con.getInputStream()));
 		String inputLine;
 		StringBuffer response = new StringBuffer();
-
 		while ((inputLine = in.readLine()) != null) {
 			response.append(inputLine);
 		}
-		in.close();
-
-		// print result
-		System.out.println(response.toString());
-		
+		in.close();		
 		//parse JSON response and return 'success' value
 		    javax.json.JsonReader jsonReader = Json.createReader(new StringReader(response.toString()));
 		    javax.json.JsonObject jsonObject = jsonReader.readObject();
